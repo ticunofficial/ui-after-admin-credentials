46d8a06b9b89bd761a14bbc9d9616420ea964b9e
abhijitugalmogale <abhiugalmogale12@gmail.com>
2025-11-17 19:08:44 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/react-build/',
-  build: {
-    outDir: '../public_html/admin/public/react-build',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 })
